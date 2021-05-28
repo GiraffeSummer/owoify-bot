e485171bf9828b1569f05831d9208e37257f05c2
GiraffeSummer <gidoselten@hotmail.com>
2021-05-28 15:35:12 +0200

    cleaned up webhookify js

--- libraries/webhookify.js
+++ libraries/webhookify.js
@@ -1,11 +1,10 @@
-module.exports = (msg, text, modifier = (s) => { return s }) => {
+const defaultModifier = (s) => { return s };
+
+const mod = async (msg, text, modifier = defaultModifier) => {
     if (text.length < 1) return;
     msg.delete();
-    msg.channel.createWebhook(msg.member.displayName, {
-        avatar: msg.author.displayAvatarURL({ dynamic: true })
-    }).then(webhook => {
-        webhook.send(modifier(text)).then(response => {
-            webhook.delete('').catch(err => { console.log(err) })
-        }).catch(err => { console.log(err) })
-    }).catch(err => { console.log(err) })
+    const hook = await msg.channel.createWebhook(msg.member.displayName, { avatar: msg.author.displayAvatarURL({ dynamic: true }) })
+    await hook.send(modifier(text));
+    hook.delete();
 }
+module.exports = mod
